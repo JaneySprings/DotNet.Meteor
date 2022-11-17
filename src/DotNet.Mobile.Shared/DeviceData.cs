f1a784703fa71ea3e065703e1022516dd30ac1c9
Nikita Romanov <nromanov@Nikitas-MacBook-Pro.local>
2022-11-17 22:56:24 +0400

    ios

--- src/DotNet.Mobile.Shared/DeviceData.cs
+++ src/DotNet.Mobile.Shared/DeviceData.cs
@@ -5,8 +5,7 @@ namespace DotNet.Mobile.Shared {
         [JsonPropertyName("name")] public string Name { get; set; }
         [JsonPropertyName("details")] public string Details { get; set; }
         [JsonPropertyName("serial")] public string Serial { get; set; }
-        [JsonPropertyName ("platform")] public string Platform { get; set; }
-        [JsonPropertyName("version")] public string Version { get; set; }
+        [JsonPropertyName("platform")] public string Platform { get; set; }
         [JsonPropertyName("is_emulator")] public bool IsEmulator { get; set; }
         [JsonPropertyName("is_running")] public bool IsRunning { get; set; }
     }
