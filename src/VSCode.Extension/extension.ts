2be6773f7a084ef4dcb81ff13b1c5a3ed3907f57
Nikita Romanov <acce.ventura@icloud.com>
2022-11-19 11:25:58 +0400

    refactoring

--- src/VSCode.Extension/extension.ts
+++ src/VSCode.Extension/extension.ts
@@ -1,47 +1,26 @@
 import * as vscode from 'vscode';
 import { Configuration } from './configuration';
-import * as constants from './constants';
-import { DebuggerUtils } from "./bridge";
+import { Command } from './constants';
 import { Interface } from './interface';
-import { Target } from './configuration';
 
 
 export function activate(context: vscode.ExtensionContext) {
+	Interface.activate();
+
 	if (vscode.workspace.workspaceFolders === undefined) 
 		return;
 
-	fetchWorkspace();
-	fetchDevices();
+	Configuration.fetchWorkspace();
+	Configuration.fetchDevices();
 	
 	if (Configuration.workspaceProjects.length === 0)
 		return;
 	
-	Interface.initStatusItemCommands();
-	Configuration.selectProject(Configuration.workspaceProjects[0]);
-	Configuration.selectTarget(Target.Debug);
-	Configuration.selectDevice(Configuration.mobileDevices[0]);
+	Configuration.selectDefaults();
 
-	context.subscriptions.push(vscode.commands.registerCommand(constants.commandSelectProjectIdentifier, () => {
-		Interface.showQuickPickProject();
-	}));
-	context.subscriptions.push(vscode.commands.registerCommand(constants.commandSelectTargetIdentifier, () => {
-		Interface.showQuickPickTarget();
-	}));
-	context.subscriptions.push(vscode.commands.registerCommand(constants.commandSelectDeviceIdentifier, () => {
-		Interface.showQuickPickDevice();
-	}));
+	context.subscriptions.push(vscode.commands.registerCommand(Command.selectProject, Interface.showQuickPickProject));
+	context.subscriptions.push(vscode.commands.registerCommand(Command.selectTarget, Interface.showQuickPickTarget));
+	context.subscriptions.push(vscode.commands.registerCommand(Command.selectDevice, Interface.showQuickPickDevice));
 }
 
 export function deactivate() {}
-
-
-function fetchWorkspace() {
-	const workspacePath = vscode.workspace.workspaceFolders![0].uri.fsPath;
-	Configuration.workspaceProjects = DebuggerUtils.findProjects(workspacePath);
-}
-
-function fetchDevices() {
-	const androidDevices = DebuggerUtils.androidDevices();
-	const appleDevices = DebuggerUtils.appleDevices();
-	Configuration.mobileDevices = androidDevices.concat(appleDevices);
-}
