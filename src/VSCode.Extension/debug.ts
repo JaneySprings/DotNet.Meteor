d77c19fd45a944470ff66e8c71fe8cb1b3b3b5c6
Nikita Romanov <acce.ventura@icloud.com>
2022-12-01 21:31:31 +0400

    fix release configuration

--- src/VSCode.Extension/debug.ts
+++ src/VSCode.Extension/debug.ts
@@ -1,5 +1,5 @@
 import { WorkspaceFolder, DebugConfiguration } from 'vscode';
-import { Configuration } from './configuration';
+import { Configuration, Target } from './configuration';
 import { ViewController } from './controller';
 import * as vscode from 'vscode';
 
@@ -7,7 +7,12 @@ import * as vscode from 'vscode';
 export class DotNetDebuggerConfiguration implements vscode.DebugConfigurationProvider {
 	async resolveDebugConfiguration(folder: WorkspaceFolder | undefined, 
 									config: DebugConfiguration, 
-									token?: vscode.CancellationToken): Promise<DebugConfiguration> {
+									token?: vscode.CancellationToken): Promise<DebugConfiguration | undefined> {
+		
+		if (config.noDebug === undefined && Configuration.selectedTarget === Target.Release) {
+			vscode.window.showErrorMessage("Debugging is not supported in release configuration");
+			return undefined;
+		}
 
 		Configuration.updateDebuggingPort();
 		ViewController.fetchDevices();
@@ -15,6 +20,7 @@ export class DotNetDebuggerConfiguration implements vscode.DebugConfigurationPro
 		
 		config['selected_project'] = Configuration.selectedProject;
         config['selected_device'] = Configuration.selectedDevice;
+		config['selected_target'] = Configuration.selectedTarget;
 		config['debugging_port'] = Configuration.debuggingPort;
 		
         return config;
