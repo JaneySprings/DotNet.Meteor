b2cbb96d369f6e9213fb51b900dd44a0f2c3dcaf
Nikita Romanov <acce.ventura@icloud.com>
2022-12-04 19:11:51 +0400

    Add hotReload plugin

---
+++ src/DotNet.Mobile.HotReload/Extension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Esp.Resources;
+using System.Reflection;
+using Microsoft.Maui.Hosting;
+
+[assembly: AssemblyMetadata("IsTrimmable", "True")]
+namespace DotNet.Mobile.HotReload;
+
+public static class Extension {
+	public static MauiAppBuilder EnableHotReload(this MauiAppBuilder builder, string? ideIp = null, int idePort = Constants.DEFAULT_PORT) {
+		builder.Services.TryAddEnumerable(ServiceDescriptor.Transient<IMauiInitializeService, HotReloadBuilder>(_ => new HotReloadBuilder {
+			IdeIp = ideIp,
+			IdePort = idePort,
+		}));
+		return builder;
+	}
+}
