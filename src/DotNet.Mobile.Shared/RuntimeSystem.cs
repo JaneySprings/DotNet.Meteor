2838cadccb068b667bca06736f03ee3369529949
Nikita Romanov <nikita.romanov@devexpress.com>
2022-12-12 11:48:17 +0400

    Fix missing windows extension for DotNet runner

--- src/DotNet.Mobile.Shared/RuntimeSystem.cs
+++ src/DotNet.Mobile.Shared/RuntimeSystem.cs
@@ -1,8 +1,15 @@
+using System;
 using System.Runtime.InteropServices;
+
 
 namespace DotNet.Mobile.Shared {
     public static class RuntimeSystem {
         public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
         public static bool IsMacOS => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static string ExecExtension => IsWindows ? ".exe" : "";
+        public static string HomeDirectory => IsWindows
+            ? Environment.GetEnvironmentVariable("HOMEPATH")
+            : Environment.GetEnvironmentVariable("HOME");
     }
 }
