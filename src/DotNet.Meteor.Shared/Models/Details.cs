aba1ed788b29ae3209ae5d32a9c71fe508e75e8c
Nikita Romanov <nikita.romanov@devexpress.com>
2023-02-08 15:37:35 +0400

    some refactoring

---
+++ src/DotNet.Meteor.Shared/Models/Details.cs
@@ -0,0 +1,11 @@
+
+namespace DotNet.Meteor.Shared {
+    public static class Details {
+        public const string AndroidEmulator = "Emulator";
+        public const string AndroidDevice = "Device";
+        public const string iOSSimulator = "iPhoneSimulator";
+        public const string iOSDevice = "iPhone";
+        public const string MacCatalyst = "Mac";
+        public const string Windows = "Windows";
+    }
+}
