5fc443e657cd2f6960d76af71839f6a4ab5f9671
Nikita Romanov <acce.ventura@icloud.com>
2023-03-07 23:20:46 +0400

    Fix breakpoints incorrect resolving. Add conditional breakpoints support

--- src/DotNet.Meteor.Debug/Protocol/Types/Breakpoint.cs
+++ src/DotNet.Meteor.Debug/Protocol/Types/Breakpoint.cs
@@ -15,18 +15,13 @@ public class Breakpoint {
     /* An optional start column of the actual range covered by the breakpoint. */
     [JsonPropertyName("column")] public int? Column { get; set; }
 
-    /* An optional end line of the actual range covered by the breakpoint. */
-    [JsonPropertyName("endLine")] public int? EndLine { get; set; }
+    /* An optional expression for conditional breakpoints. */
+    [JsonPropertyName("condition")] public string Condition { get; set; }
 
-    /* An optional end column of the actual range covered by the breakpoint. If no
-    * end line is given, then the end column is assumed to be in the start line. */
-    [JsonPropertyName("endColumn")] public int? EndColumn { get; set; }
-
-    public Breakpoint(bool verified, int line, int? column = null, int? endLine = null, int? endColumn = null) {
+    public Breakpoint() { }
+    public Breakpoint(bool verified, int line, int? column = null) {
         this.Verified = verified;
         this.Line = line;
         this.Column = column;
-        this.EndLine = endLine;
-        this.EndColumn = endColumn;
     }
 }
