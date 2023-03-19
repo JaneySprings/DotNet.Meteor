52f8029c72606b33db8362e0e3715bf7b07dd154
Nikita Romanov <acce.ventura@icloud.com>
2023-03-19 20:00:55 +0400

    Add method for unsubscribing from an event handler

--- src/VSCode.Extension/exports.ts
+++ src/VSCode.Extension/exports.ts
@@ -24,6 +24,11 @@ class EventHandler {
     public add(callback: (data: any) => void) {
         this.callbacks.push(callback);
     }
+    public remove(callback: (data: any) => void) {
+        const index = this.callbacks.indexOf(callback);
+        if (index != -1 && index < this.callbacks.length)
+            this.callbacks.splice(index, 1);
+    }
     public invoke(data: any) {
         this.callbacks.forEach(callback => callback(data));
     }
