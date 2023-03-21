e55fd99ad1549495c1581f5f6ef8d7ddd57251d3
Nikita Romanov <nikita.romanov@devexpress.com>
2023-03-21 22:09:50 +0400

    Impove stackFrames markers. Improve 'Next' experience

--- src/DotNet.Meteor.Debug/Protocol/Types/StackFrame.cs
+++ src/DotNet.Meteor.Debug/Protocol/Types/StackFrame.cs
@@ -18,6 +18,14 @@ public class StackFrame {
     * and must be ignored. */
     [JsonPropertyName("column")] public int Column { get; set; }
 
+    /* The end line of the range covered by the stack frame. */
+    [JsonPropertyName("endLine")] public int EndLine { get; set; }
+
+    /* End position of the range covered by the stack frame. It is measured in
+    * UTF-16 code units and the client capability `columnsStartAt1` determines
+    * whether it is 0- or 1-based. */
+    [JsonPropertyName("endColumn")] public int EndColumn { get; set; }
+
     /* The name of the stack frame, typically a method name. */
     [JsonPropertyName("name")] public string Name { get; set; }
 
@@ -28,7 +36,7 @@ public class StackFrame {
     * Values: 'normal', 'label', 'subtle'. */
     [JsonPropertyName("presentationHint")] public string PresentationHint { get; set; }
 
-    public StackFrame(int id, string name, Source source, int line, int column, string hint) {
+    public StackFrame(int id, Source source, string hint, string name, int line, int column, int endLine, int endColumn) {
         this.Id = id;
         this.Name = name;
         this.Source = source;
@@ -36,6 +44,8 @@ public class StackFrame {
         // These should NEVER be negative
         this.Line = System.Math.Max(0, line);
         this.Column = System.Math.Max(0, column);
+        this.EndLine = System.Math.Max(0, endLine);
+        this.EndColumn = System.Math.Max(0, endColumn);
 
         this.PresentationHint = hint;
     }
