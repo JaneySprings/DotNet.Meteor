331596a50cd921c7be37200b01e7db5a0079baa2
Nikita Romanov <nikita.romanov@devexpress.com>
2023-04-13 19:42:34 +0400

    Add arm simulator support

--- src/DotNet.Meteor.Shared/Apple/AppleTool.cs
+++ src/DotNet.Meteor.Shared/Apple/AppleTool.cs
@@ -9,6 +9,9 @@ namespace DotNet.Meteor.Apple {
         public static List<DeviceData> VirtualDevices() {
             var devices = new List<DeviceData>();
             var path = PathUtils.SimulatorsLocation();
+            var runtimeId = SystemProfiler.IsArch64() 
+                ? Runtimes.iOSSimulatorArm64 
+                : Runtimes.iOSSimulatorX64;
 
             foreach (string directory in Directory.GetDirectories(path)) {
                 var plist = Path.Combine(directory, "device.plist");
@@ -17,18 +20,15 @@ namespace DotNet.Meteor.Apple {
                     continue;
 
                 var extractor = new PropertyExtractor(plist);
-
                 if (extractor.ExtractBoolean("isDeleted"))
                     continue;
 
                 var runtime = extractor.Extract("runtime");
                 var osVersion = "Unknown";
-
                 if (runtime?.Contains("SimRuntime.iOS") != true)
                     continue;
 
                 var tokens = runtime.Split('.').Last().Split('-');
-
                 if (tokens.Length > 1)
                     osVersion = $"{tokens[0]} {string.Join('.', tokens.Skip(1))}";
 
@@ -37,7 +37,7 @@ namespace DotNet.Meteor.Apple {
                     IsMobile = true,
                     IsRunning = extractor.Extract("state", "integer")?.Equals("3") == true,
                     Name = extractor.Extract("name") ?? "Unknown",
-                    RuntimeId = Runtimes.iOSimulatorX64,
+                    RuntimeId = runtimeId,
                     Details = Details.iOSSimulator,
                     Platform = Platforms.iOS,
                     OSVersion = osVersion,
