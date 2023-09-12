30a9a4334d6b00a66f113a652c0ce20fece83b4e
Nikita Romanov <acce.ventura@icloud.com>
2023-09-12 19:48:27 +0400

    Fix missing log initializer

--- src/DotNet.Meteor.Debug/Main.cs
+++ src/DotNet.Meteor.Debug/Main.cs
@@ -4,6 +4,7 @@ namespace DotNet.Meteor.Debug;
 
 public class Program {
     public static void Main(string[] args) {
+        Logging.LogConfig.InitializeLog();
         var debugSession = new DebugSession(Console.OpenStandardInput(), Console.OpenStandardOutput());
         debugSession.Start();
     }
