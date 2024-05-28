af112472091c0991c84a8bbb0f7f669cb7ae14e1
Nikita Romanov <acce.ventura@icloud.com>
2024-05-28 15:26:25 +0400

    Fix android HotReload issues

--- src/DotNet.Meteor.Debug/Agents/BaseLaunchAgent.cs
+++ src/DotNet.Meteor.Debug/Agents/BaseLaunchAgent.cs
@@ -17,7 +17,7 @@ public abstract class BaseLaunchAgent {
     protected HotReloadClient HotReloadClient { get; init; }
 
     protected BaseLaunchAgent(LaunchConfiguration configuration) {
-        HotReloadClient = new HotReloadClient();
+        HotReloadClient = new HotReloadClient(configuration.ReloadHostPort, configuration.Device.IsAndroid);
         Disposables = new List<Action>();
         Configuration = configuration;
     }
@@ -27,16 +27,16 @@ public abstract class BaseLaunchAgent {
 
     public virtual List<CompletionItem> GetCompletionItems() => new List<CompletionItem>();
     public virtual void HandleCommand(string command, IProcessLogger logger) { }
-    public virtual void ConnectHotReload(int port) {
+    public virtual void ConnectHotReload() {
         Disposables.Add(() => HotReloadClient.Close());
-        _ = HotReloadClient.TryConnectAsync(port);
+        _ = HotReloadClient.TryConnectAsync();
     }
     public virtual void SendHotReloadNotification(string filePath, IProcessLogger logger = null) {
-        if (HotReloadClient.IsSupported && !HotReloadClient.IsRunning) {
-            logger.OnErrorDataReceived("Hot reload client not connected");
-            return;
+        try {
+            HotReloadClient.SendNotification(filePath, logger);
+        } catch (Exception ex) {
+            logger.OnErrorDataReceived($"[HotReload]: {ex.Message}");
         }
-        HotReloadClient.SendNotification(filePath, logger);
     }
     public virtual void Dispose() {
         foreach (var disposable in Disposables) {
