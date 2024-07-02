ceccc26f08c336e3e4e325234295d87d74264af4
Nikita Romanov <nikita.romanov@devexpress.com>
2024-07-02 23:18:16 +0400

    Add additional params

--- src/DotNet.Meteor.Debug/Sdk/Profiling/GCDump.cs
+++ src/DotNet.Meteor.Debug/Sdk/Profiling/GCDump.cs
@@ -17,21 +17,28 @@ public static class GCDump {
         return new FileInfo(path);
     }
 
-    public static Process Collect(int pid, string outputFile, IProcessLogger logger) {
+    public static Process Collect(int pid, string outputFile, string additionalArgs, IProcessLogger logger) {
         var gcdump = GCDump.GCDumpTool();
         var arguments = new ProcessArgumentBuilder()
             .Append("collect")
             .Append("-p", pid.ToString())
             .Append("-o").AppendQuoted(outputFile);
+
+        if (!string.IsNullOrEmpty(additionalArgs))
+            arguments.Append(additionalArgs);
+
         return new ProcessRunner(gcdump, arguments, logger).Start();
     }
-    public static Process Collect(string diagnosticPort, string outputFile, IProcessLogger logger) {
+    public static Process Collect(string diagnosticPort, string outputFile, string additionalArgs, IProcessLogger logger) {
         var gcdump = GCDump.GCDumpTool();
         var arguments = new ProcessArgumentBuilder()
             .Append("collect")
             .Append("--dport", diagnosticPort)
-            .Append("-o").AppendQuoted(outputFile)
-            .Append("-v");
+            .Append("-o").AppendQuoted(outputFile);
+
+        if (!string.IsNullOrEmpty(additionalArgs))
+            arguments.Append(additionalArgs);
+
         return new ProcessRunner(gcdump, arguments, logger).Start();
     }
 }
