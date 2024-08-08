915a684eca7de64b213ae19f547f33e6d6e06e15
Nikita Romanov <nikita.romanov@devexpress.com>
2024-08-08 21:30:18 +0400

    [Profiler] Fix android hangs (#112)

--- src/DotNet.Meteor.Debug/Sdk/Profiling/Trace.cs
+++ src/DotNet.Meteor.Debug/Sdk/Profiling/Trace.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using DotNet.Meteor.Debug.Extensions;
 using DotNet.Meteor.Processes;
 using TraceCollectHandler = Microsoft.Diagnostics.Tools.Trace.CollectCommandHandler;
@@ -18,27 +19,29 @@ public static class Trace {
     private static ProfilerTask CollectCore(int pid, string diagnosticPort, string outputFile, IProcessLogger logger) {
         var cancellationTokenSource = new CancellationTokenSource();
         var token = cancellationTokenSource.Token;
-        var task = TraceCollectHandler.Collect(
-            ct: token,
-            console: new ConsoleLogger(logger),
-            processId: pid,
-            output: new FileInfo(outputFile),
-            buffersize: TraceCollectHandler.DefaultCircularBufferSizeInMB(),
-            providers: string.Empty,
-            profile: string.Empty,
-            format: Microsoft.Diagnostics.Tools.Trace.TraceFileFormat.Speedscope,
-            duration: default,
-            clrevents: string.Empty,
-            clreventlevel: string.Empty,
-            name: null,
-            diagnosticPort: diagnosticPort,
-            showchildio: false,
-            resumeRuntime: true,
-            stoppingEventProviderName: null,
-            stoppingEventEventName: null,
-            stoppingEventPayloadFilter: null,
-            rundown: null
-        );
+        var task = Task.Run(async() => {
+            await TraceCollectHandler.Collect(
+                ct: token,
+                console: new ConsoleLogger(logger),
+                processId: pid,
+                output: new FileInfo(outputFile),
+                buffersize: TraceCollectHandler.DefaultCircularBufferSizeInMB(),
+                providers: string.Empty,
+                profile: string.Empty,
+                format: Microsoft.Diagnostics.Tools.Trace.TraceFileFormat.Speedscope,
+                duration: default,
+                clrevents: string.Empty,
+                clreventlevel: string.Empty,
+                name: null,
+                diagnosticPort: diagnosticPort,
+                showchildio: false,
+                resumeRuntime: true,
+                stoppingEventProviderName: null,
+                stoppingEventEventName: null,
+                stoppingEventPayloadFilter: null,
+                rundown: null
+            );
+        });
 
         return new ProfilerTask(task, cancellationTokenSource);
     }
