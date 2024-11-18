1350b525c567a8fad745bfca85daf4d3e064b8bd
Nikita Romanov <acce.ventura@icloud.com>
2024-11-18 23:12:46 +0400

    Revert "Remove public exports. Add `DotRush` status bar integration"
    This reverts commit cb2e3c59eb730c0b6f0b4e20a748c68c35c04ab6.

--- src/VSCode.Extension/extension.ts
+++ src/VSCode.Extension/extension.ts
@@ -4,6 +4,7 @@ import { ConfigurationController } from './controllers/configurationController';
 import { StatusBarController } from './controllers/statusbarController';
 import { Interop } from './interop/interop';
 import { StateController } from './controllers/stateController';
+import { PublicExports } from './publicExports';
 import { ModulesView } from './features/modulesView';
 import { MauiEssentials } from './features/mauiEssentials';
 import { ExternalTypeResolver } from './features/externalTypeResolver';
@@ -12,21 +13,27 @@ import * as res from './resources/constants';
 import * as vscode from 'vscode';
 
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): PublicExports | undefined {
 	Interop.initialize(context.extensionPath);
 
 	if (vscode.workspace.workspaceFolders === undefined) 
 		return undefined;
+
+	const exports = new PublicExports();
 	
 	ConfigurationController.activate(context);
 	StateController.activate(context);
 	StatusBarController.activate(context);
+	StatusBarController.update();
 
 	ModulesView.feature.activate(context);
 	MauiEssentials.feature.activate(context);
 	ExternalTypeResolver.feature.activate(context);
 	RemoteHostProvider.feature.activate(context);
 	
+	context.subscriptions.push(vscode.commands.registerCommand(res.commandIdSelectActiveProject, StatusBarController.showQuickPickProject));
+	context.subscriptions.push(vscode.commands.registerCommand(res.commandIdSelectActiveConfiguration, StatusBarController.showQuickPickConfiguration));
+	context.subscriptions.push(vscode.commands.registerCommand(res.commandIdSelectActiveDevice, StatusBarController.showQuickPickDevice));
 	context.subscriptions.push(vscode.commands.registerCommand(res.commandIdActiveTargetFramework, () => ConfigurationController.getTargetFramework()));
 	context.subscriptions.push(vscode.commands.registerCommand(res.commandIdActiveConfiguration, () => ConfigurationController.configuration));
 	context.subscriptions.push(vscode.commands.registerCommand(res.commandIdActiveProjectPath, () => ConfigurationController.project?.path));
@@ -35,9 +42,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider(res.debuggerMeteorId, new MonoDebugConfigurationProvider()));
 	context.subscriptions.push(vscode.tasks.registerTaskProvider(res.taskDefinitionId, new DotNetTaskProvider()));
+
+	return exports;
 }
 
 export function deactivate() {
 	StateController.deactivate();
-	StatusBarController.deactivate();
 }
